6061e44fc2e37ac3a6011262e7bed838743f51fd
Francisco <102423868+FranciscoAraujo2@users.noreply.github.com>
2022-07-02 13:24:01 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from pypresence import *
+import time
+
+
+ID = '' #https://discord.com/developers/applications
+
+
+DETAILS = '🍓・Rpc made by KIKO#5148 '
+STATE = "🫐・Houston, we've had a problem."
+LOGO = 'logo'
+
+#Buttons 
+LABEL1 = '🐈 Github'
+URL1 = 'https://github.com/FranciscoAraujo2'
+LABEL2 = '📷 Instagram'
+URL2 = 'https://www.instagram.com/francisco.araujo_2/'
+
+
+#Dont touch
+
+RPC = Presence(ID)
+RPC.connect()
+
+while True:
+    RPC.update(
+        details = DETAILS,
+        large_image = LOGO, 
+        buttons =[{"label": LABEL1, "url": URL1},{"label": LABEL2, "url": URL2}],
+        state = STATE
+    )
+    time.sleep(600000)
+    print('Rpc made by KIKO#5148')
